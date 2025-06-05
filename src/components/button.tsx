fd960711f6ab785d39d6e6312ad7668f862d36a0
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-05 18:05:50 +0200

    feat: add button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,23 @@
-export default function Button() {
+import { Mode } from "../types/mode";
+
+type ButtonProps = {
+  change: () => void;
+  mode: Mode;
+};
+
+export default function Button({ change, mode }: ButtonProps) {
   return (
     <>
-      <button className="btn btn-primary m-3 ">Change</button>
+      <button
+        className={
+          mode === Mode.light
+            ? "btn btn-primary m-3 "
+            : "btn btn-danger text-with m-3 "
+        }
+        onClick={change}
+      >
+        Change
+      </button>
     </>
   );
 }
