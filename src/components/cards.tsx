f05ec5630c466b9200ab241ed07a67ca1ad4fcaa
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-05 17:21:58 +0200

    feat: add cards

---
+++ src/components/cards.tsx
@@ -0,0 +1,54 @@
+type ElementType = {
+  title: string;
+  description: string;
+};
+
+const element: ElementType[] = [
+  {
+    title: "magnam facilis autem",
+    description:
+      "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    title: "qui est esse",
+    description:
+      "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla",
+  },
+  {
+    title: "ea molestias quasi",
+    description:
+      "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut",
+  },
+  {
+    title: "eum et est occaecati",
+    description:
+      "ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit",
+  },
+  {
+    title: "nesciunt quas odio",
+    description:
+      "repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque",
+  },
+];
+
+export default function Cards() {
+  return (
+    <>
+      <div className="container">
+        <div className="row d-flex justify-content-center ">
+          {element.map((el) => (
+            <>
+              <div className="card  col-5  m-1 ">
+                <div className="card-body d-flex flex-column justify-content-start align-items-center">
+                  <h6 className="card-title text-uppercase">{el.title}</h6>
+                  <hr className="border border-primary border-3 w-25" />
+                  <p className="card-text">{el.description}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
